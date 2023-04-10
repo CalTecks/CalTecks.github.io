825ebc49ee3ffb6933b06e066874119753fef066
ThomasVB <thomas.van.beeck@pandora.be>
2023-04-10 12:07:52 +0200

    prototype finished

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def splitContent(filename, type="markdown", path="./pages/"):
         return False
     if type == "yaml":
         return yaml.safe_load(content[start+3:end])
-    return markdown.markdown(content[end+3:])
+    else:
+        return markdown.markdown(content[end+3:])
 
 
 def getTemplate(filename):
@@ -25,14 +26,18 @@ def getTemplate(filename):
     return template
 
 
-def writeHTML(filename, template, content):
+def writeHTML(template, content):
     print(colored(content, 'magenta'))
     output = template.render(content)
     print()
     print()
     print(colored(output, 'grey'))
+    return output
+
+
+def saveHTML(filename, content):
     save_html = open("./_site/"+filename+".html", "w")
-    for line in output:
+    for line in content:
         save_html.write(line)
     save_html.close()
 
@@ -42,42 +47,41 @@ def processContent(filename):
     markd_content = splitContent(filename)
     content["main_content"] = markd_content
     template = getTemplate(filename)
-    print(content["type"])
+    print(colored(content["type"], 'red'))
     if content["type"] == "blog":
         print(colored("blog!", "red"))
-        blog_content = getBlogPosts()
-        content_list = []
-        content_list.append(content)
-        content_list.extend(blog_content)
-        writeHTML(filename, template, content_list)
+        full_blog_content = getBlogPosts()
+        content["main_blog_content"] = full_blog_content
+        contentHTML = writeHTML(template, content)
+        saveHTML(filename, contentHTML)
     else:
-        writeHTML(filename, template, content)
+        contentHTML = writeHTML(template, content)
+        saveHTML(filename, contentHTML)
 
 
 def getBlogPosts():
     # list maken van blogpost dictionaries
-    bloglist = []
-    files_in_posts = Path("./posts/").iterdir()
+    posts_list = scanFiles("./posts/")
+    template = getTemplate("post")
+    fullblogHTML = ""
     counter = 0
-    for item in files_in_posts:
-        if item.is_file():
-            file_name = item.name
-            base_name, extension = os.path.splitext(file_name)
-            yaml_content = splitContent(
-                base_name, type="yaml", path="./posts/")
-            markd_content = splitContent(
-                base_name, type="markdown", path="./posts/")
-            yaml_content["main_content"] = markd_content
-            bloglist.append(yaml_content)
-            counter += 1
+    for item in posts_list:
+        yaml_content = splitContent(
+            item, type="yaml", path="./posts/")
+        markd_content = splitContent(
+            item, type="markdown", path="./posts/")
+        yaml_content["post_content"] = markd_content
+        blogpostHTML = writeHTML(template, yaml_content)
+        fullblogHTML += blogpostHTML
+        counter += 1
     print(f"getBlogPosts(): {counter} blog posts found")
-    print(colored(bloglist, 'blue'))
-    return bloglist
+    print(colored(fullblogHTML, 'blue'))
+    return fullblogHTML
 
 
-def scanFiles():
+def scanFiles(dir):
     filename_list = []
-    files_in_dir = Path("./pages/").iterdir()
+    files_in_dir = Path(dir).iterdir()
     for item in files_in_dir:
         if item.is_file():
             file_name = item.name
@@ -87,6 +91,10 @@ def scanFiles():
     return filename_list
 
 
-filename_list = scanFiles()
-for file in filename_list:
-    processContent(file)
+def processAll():
+    filename_list = scanFiles("./pages")
+    for file in filename_list:
+        processContent(file)
+
+
+processAll()
